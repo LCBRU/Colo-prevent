d9be7d96f0d55f65b4c16cc147e61468aa9936b1
Natasha B <97158331+Tash91@users.noreply.github.com>
2024-06-07 16:03:10 +0100

    Add files via upload

--- COLO_PREV WEB APP/web_app.py
+++ COLO_PREV WEB APP/web_app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template,redirect
+from flask import Flask, render_template,redirect, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, IntegerField
+from wtforms import StringField, DateField, IntegerField,SubmitField,FieldList,FormField
 from wtforms.validators import DataRequired, Optional
 import os
 SECRET_KEY = os.urandom(32)
@@ -10,6 +10,14 @@ class SupplyForm(FlaskForm):
     equipment = StringField('equipment', validators=[Optional()])
     batch_number = IntegerField('batch_number', validators=[Optional()])
     disposal_date = DateField('disposal_date', validators=[Optional()])
+  
+
+    
+class AdditonalForm(FlaskForm):
+    more_fields = FieldList(FormField(SupplyForm), min_entries=4)
+    submit = SubmitField('submit')
+    edit = SubmitField('edit')
+    delete = SubmitField('delete')
 
 
 app = Flask(__name__)
@@ -18,7 +26,7 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
-    form = SupplyForm()
+    form = AdditonalForm()
     if form.validate_on_submit():
         return redirect('/success')
 
@@ -27,3 +35,4 @@ def submit():
 @app.route('/success', methods=['GET', 'POST'])
 def success():
     return render_template('success.html')
+
